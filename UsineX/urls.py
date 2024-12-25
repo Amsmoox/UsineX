d90d72089c599d9c23c0e6a26676d01397c68230
amsmoox <amsmoox@gmail.com>
2024-12-25 21:10:34 +0100

    link main urls with users

--- UsineX/urls.py
+++ UsineX/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from users.views import root_redirect
 
 urlpatterns = [
+    path('', root_redirect, name='root'),
     path('admin/', admin.site.urls),
-    path('machines/', include('Machines.urls')),  
+    path('machines/', include('Machines.urls')), 
+    path('users/', include('users.urls')), 
+    
 ]
