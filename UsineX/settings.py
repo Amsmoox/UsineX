bde2d23cca8c27fb2700184a927a57db7b8fc72f
amsmoox <amsmoox@gmail.com>
2024-12-25 21:10:20 +0100

    link users app with settings

--- UsineX/settings.py
+++ UsineX/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Machines',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'UsineX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "UsineX",
+        'USER': "postgres",
+        'PASSWORD': "Mohylah2@",
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -130,3 +135,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
