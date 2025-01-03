d3ec446284f19f18644dc1fa6306e77ae2c6008e
amsmoox <amsmoox@gmail.com>
2025-01-03 12:11:36 +0100

    update my urls

--- Machines/urls.py
+++ Machines/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MachineListView, MachineDetailView, MachineFormView, MachineDeleteView
+from .views import MachineListView, MachineDetailView, MachineFormView, MachineDeleteView, ProductionCreateView, get_materials, get_materials_and_colors
 
 urlpatterns = [
     path('', MachineListView.as_view(), name='machine_list'),               # hadi pour list all active machines
@@ -7,4 +7,8 @@ urlpatterns = [
     path('create/', MachineFormView.as_view(), name='machine_create'),
     path('<int:pk>/update/', MachineFormView.as_view(), name='machine_update'), 
     path('<int:pk>/delete/', MachineDeleteView.as_view(), name='machine_delete'),
+
+
+    path('production/', ProductionCreateView.as_view(), name='production_create'), 
+    path('materials/<int:machine_id>/', get_materials_and_colors, name='get_materials_and_colors'),
 ]
